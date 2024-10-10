e822e06e28c9caff337534608f2c078da1faf90c
be-great <begreat5333@gmail.com>
2024-10-10 18:19:44 +0300

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+ 0. Basic annotations - concat
+"""
+import math
+
+
+def to_str(n: float) -> str:
+    """
+    function that string the number
+    """
+    return str(n)
