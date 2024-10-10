aaa1b0f5f4a272f088fcff969444f2ee477d3a53
be-great <begreat5333@gmail.com>
2024-10-10 20:37:21 +0300

    task 8

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+ Complex types - list of floats
+"""
+from typing import List, Union, Tuple, Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    function returns a multipler
+    """
+    def fun(n: float) -> float:
+        return n * multiplier
+
+    return fun
