b26d3c0a0de5e4e9148314a56193273ae841e15c
be-great <begreat5333@gmail.com>
2024-10-10 18:30:41 +0300

    update task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+ Complex types - list of floats
+"""
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    function that sum the floating number
+    """
+    return sum(input_list)
