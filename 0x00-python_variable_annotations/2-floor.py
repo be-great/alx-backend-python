7ac53e5bbfde05468d0f360c2b41e8db939c0f84
be-great <begreat5333@gmail.com>
2024-10-10 18:16:09 +0300

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+ 0. Basic annotations - concat
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    function that floor the number
+    """
+    return math.floor(n)
