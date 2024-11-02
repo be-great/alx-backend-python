f77405b09d7288fa8fe52c9298f087e527a94ed2
be-great <begreat5333@gmail.com>
2024-11-02 18:28:22 +0300

    task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -27,7 +27,9 @@ class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
         ({"a": 1}, ("a",), 1),
         ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2)
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
     ])
     def test_access_nested_map(self, nested_map, path, expected):
         """
@@ -39,6 +41,7 @@ class TestAccessNestedMap(unittest.TestCase):
         """parameterize"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
 
 
 if __name__ == "__main__":
