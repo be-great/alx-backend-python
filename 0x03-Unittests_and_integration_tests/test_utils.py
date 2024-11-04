eacb2a776ff2e6b81db2268fea1ca26c576f616d
be-great <begreat5333@gmail.com>
2024-11-04 20:24:56 +0300

    update task 1

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -35,15 +35,15 @@ class TestAccessNestedMap(unittest.TestCase):
         """
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-        @parameterized.expand([
-            ({}, ("a",), 'a'),
-            ({"a": 1}, ("a", "b"), 'b')
-        ])
-        def test_access_nested_map_exception(self, nested_map, path, expected):
-            """ Test that a KeyError is raised for the respective inputs """
-            with self.assertRaises(KeyError) as e:
-                access_nested_map(nested_map, path)
-            self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
+    @parameterized.expand([
+        ({}, ("a",), 'a'),
+        ({"a": 1}, ("a", "b"), 'b')
+    ])
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """ Test that a KeyError is raised for the respective inputs """
+        with self.assertRaises(KeyError) as e:
+            access_nested_map(nested_map, path)
+        self.assertEqual(f"KeyError('{expected}')", repr(e.exception))
 
 
 class TestGetJson(unittest.TestCase):
